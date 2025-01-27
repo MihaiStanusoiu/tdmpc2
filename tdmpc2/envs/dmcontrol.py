9cdbad70886fd92ddc6245d2915222e147ff5ad4
Mihai-Teodor Stanusoiu <stanusoiumihai@gmail.com>
2025-01-27 19:30:51 +0100

    Safety guard for physics perturbation

--- tdmpc2/envs/dmcontrol.py
+++ tdmpc2/envs/dmcontrol.py
@@ -187,7 +187,7 @@ class TimeStepToGymWrapper:
 	
 	def step(self, action):
 		if self.delay_enabled:
-			self.dm_env._n_sub_steps = int(np.round(np.random.normal(self.delay_mu, self.delay_mu * self.delay_sigma, 1))[0])
+			self.dm_env._n_sub_steps = max(0, int(np.round(np.random.normal(self.delay_mu, self.delay_mu * self.delay_sigma, 1))[0]))
 		self.t += 1
 		self.substeps += self.dm_env._n_sub_steps
 		timestamp = self.t * self.dm_env._n_sub_steps * self.dm_env._n_sub_steps * self.dm_env._physics.timestep()
