affbeb2b370b8a8b4d3a163fcdb1c184e58e0ee9
Mihai-Teodor Stanusoiu <stanusoiumihai@gmail.com>
2025-04-26 13:49:45 +0200

    Fixed compat with older models

--- tdmpc2/common/world_model.py
+++ tdmpc2/common/world_model.py
@@ -44,9 +44,9 @@ class WorldModel(nn.Module):
 
 		# Assign params to modules
 		# We do this strange assignment to avoid having duplicated tensors in the state-dict -- working on a better API for this
-		delattr(self._detach_Qs, "params")
+		# delattr(self._detach_Qs, "params")
 		self._detach_Qs.__dict__["params"] = self._detach_Qs_params
-		delattr(self._target_Qs, "params")
+		# delattr(self._target_Qs, "params")
 		self._target_Qs.__dict__["params"] = self._target_Qs_params
 
 	def __repr__(self):
